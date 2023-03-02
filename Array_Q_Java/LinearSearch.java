ed48edd6f0635fdc9ca27608f588f44e80eb9a46
mishrasudheer675 <mishrasudheer109@gmail.com>
2023-03-02 17:14:45 +0530

    linearSearch

---
+++ Array_Q_Java/LinearSearch.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class LinearSearch{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the size of array: ");
+        int n = in.nextInt();
+        System.out.println("Enter the element in array: ");
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = in.nextInt();
+        }
+        System.out.print("Enter the Search Element:");
+        int search = in.nextInt();
+        int ans =  l_Search(arr,search);
+        if(ans == -1){
+        System.out.println("Element not found:");
+        }
+        else{
+            System.out.println("Element found at index: "+ans);
+        }
+    }
+    public static int l_Search(int arr[],int search){
+          for(int i=0;i<arr.length;i++){
+            if(arr[i] == search){
+                return i;
+            }
+          }
+          return -1;
+    }
+}
