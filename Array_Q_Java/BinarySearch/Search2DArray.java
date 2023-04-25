e8d3ff98def4acd991733285c511603d11e0a77d
Sudhir Mishra <mishrasudheer109@gmail.com>
2023-04-25 11:13:46 +0530

    search2DArray

--- Array_Q_Java/BinarySearch/Search2DArray.java
+++ Array_Q_Java/BinarySearch/Search2DArray.java
@@ -6,11 +6,13 @@ public class Search2DArray{
             {70,100,110}
         };
         int target = 100;
-        int ans[] = search2Darray(arr,target);
-        System.out.println(Arrays.toString(ans));
+        int[] ans = search2Darray(arr,target);
+        for(int i=0;i<ans.length;i++){
+            System.out.print(ans[i]+" ");
+        }
 
     }
-    public static int search2Darray(int[][] arr, int target){
+    public static int[] search2Darray(int[][] arr, int target){
         for(int row=0;row<arr.length;row++){
             for(int col=0;col<arr[row].length;col++){
                 if(arr[row][col]==target){
@@ -18,6 +20,6 @@ public class Search2DArray{
                 }
             }
         }
-        return new int[]{row,col};
+        return new int[]{-1,-1};
     }
 }
