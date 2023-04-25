aac8865bf80f0bb61ef3e31aa2895f33a36f970e
Sudhir Mishra <mishrasudheer109@gmail.com>
2023-04-26 00:07:41 +0530

    missingElement

--- Array_Q_Java/ArraySorting/MissingElement_UsingCyclicSort.java
+++ Array_Q_Java/ArraySorting/MissingElement_UsingCyclicSort.java
@@ -1,8 +1,10 @@
+// find the missing number in array using cyclic sort.
+// leetcode problem:- 268.
 public class MissingElement_UsingCyclicSort {
     public static void main(String[] args){
         int[] arr = {3,0,1};
         int ans = missing_Number(arr);
-        System.out.println("the missing number is:"+ans);
+        System.out.println("the missing number:"+ans);
     }
     public static int missing_Number(int[] arr){
         int i = 0;
@@ -20,9 +22,10 @@ public class MissingElement_UsingCyclicSort {
         }
         for(int j=0;j<arr.length;j++){
             if(arr[j] != j){
-                return j;
+               return j;
             }
         }
         return arr.length;
     }
 }
+
