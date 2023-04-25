bc7103fc34f5949478d7f02a1fcfa60479d5d259
Sudhir Mishra <mishrasudheer109@gmail.com>
2023-04-25 18:10:59 +0530

    missingNumber

---
+++ Array_Q_Java/ArraySorting/MissingElement_UsingCyclicSort.java
@@ -0,0 +1,28 @@
+public class MissingElement_UsingCyclicSort {
+    public static void main(String[] args){
+        int[] arr = {3,0,1};
+        int ans = missing_Number(arr);
+        System.out.println("the missing number is:"+ans);
+    }
+    public static int missing_Number(int[] arr){
+        int i = 0;
+        while(i<arr.length){
+            int position = arr[i];
+            if(arr[i] < arr.length && position != i){
+                int temp = arr[position];
+                arr[position] = arr[i];
+                arr[i] = temp;
+                
+            }
+            else{
+                i++;
+            }
+        }
+        for(int j=0;j<arr.length;j++){
+            if(arr[j] != j){
+                return j;
+            }
+        }
+        return arr.length;
+    }
+}
